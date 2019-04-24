a9a6a9340d2195b21a99a197d1a8d6a3d16839b4
Faizaakmal <49012586+Faizaakmal@users.noreply.github.com>
2019-04-24 20:51:38 +0700

    Update lib.rs

--- gigasecond/src/lib.rs
+++ gigasecond/src/lib.rs
@@ -5,4 +5,4 @@ use chrono::{DateTime, Duration, Utc};
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
     return start + Duration::seconds(1_000_000_000);
 }
-//https://exercism.io/my/solutions/8a4cdac0d1ee44918b2832e2da7f4dae
+// https://exercism.io/my/solutions/8a4cdac0d1ee44918b2832e2da7f4dae
